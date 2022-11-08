e9f911c6401185db7b438c2dc25b451becd7e3e9
Marco Buratto <m.buratto@lumit.it>
2022-11-08 14:56:56 +0100

    Controllers refactoring: Nat rulebase

--- checkpoint/controllers/CheckPoint/NatRulebase.py
+++ checkpoint/controllers/CheckPoint/NatRulebase.py
@@ -1,128 +1,55 @@
-import uuid
-import time
-
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 
 from checkpoint.models.CheckPoint.PolicyPackage import PolicyPackage
-from checkpoint.models.CheckPoint.Session import Session
-from checkpoint.models.Permission.Permission import Permission
-
-from checkpoint.controllers.CustomControllerBase import CustomControllerBase
 
 from checkpoint.serializers.CheckPoint.NatRuleBase import CheckPointNatRuleBaseSerializer as Serializer
 
-from checkpoint.helpers.Lock import Lock
-from checkpoint.helpers.Conditional import Conditional
-from checkpoint.helpers.Log import Log
+
+from checkpoint.controllers.CustomControllerGet import CustomControllerCheckPointGetList
+from checkpoint.controllers.CustomControllerPost import CustomControllerCheckPointCreate
 
 
-class CheckPointNatRulebaseController(CustomControllerBase):
+class CheckPointNatRulebaseController(CustomControllerCheckPointGetList, CustomControllerCheckPointCreate):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.sessionId = uuid.uuid4().hex
+        super().__init__(subject="nat_rulebase", *args, **kwargs)
 
 
 
     def get(self, request: Request, assetId: int, domain: str, packageUid: str) -> Response:
-        data = dict()
-        etagCondition = {"responseEtag": ""}
-        user = CustomControllerBase.loggedUser(request)
+        def actionCallback():
+            localOnly = False
+            if "local" in request.GET:
+                localOnly = True
 
-        localOnly = False
-        if "local" in request.GET:
-            localOnly = True
+            return PolicyPackage.listNatRules(sessionId="", assetId=assetId, domain=domain, policyPackageUid=packageUid, localOnly=localOnly)
 
-        try:
-            if Permission.hasUserPermission(groups=user["groups"], action="nat_rulebase_get", assetId=assetId, domain=domain) or user["authDisabled"]:
-                Log.actionLog("NAT rulebases list", user)
-
-                lock = Lock("nat_rulebase", locals())
-                if lock.isUnlocked():
-                    lock.lock()
-
-                    data = {
-                        "data": {
-                            "items": PolicyPackage.listNatRules(sessionId="", assetId=assetId, domain=domain, policyPackageUid=packageUid, localOnly=localOnly)
-                        },
-                        "href": request.get_full_path()
-                    }
-
-                    # Check the response's ETag validity (against client request).
-                    conditional = Conditional(request)
-                    etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
-                    if etagCondition["state"] == "fresh":
-                        data = None
-                        httpStatus = status.HTTP_304_NOT_MODIFIED
-                    else:
-                        httpStatus = status.HTTP_200_OK
-
-                    lock.release()
-                else:
-                    data = None
-                    httpStatus = status.HTTP_423_LOCKED
-            else:
-                data = None
-                httpStatus = status.HTTP_403_FORBIDDEN
-        except Exception as e:
-            Lock("nat_rulebase", locals()).release()
-
-            data, httpStatus, headers = CustomControllerBase.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(data, status=httpStatus, headers={
-            "ETag": etagCondition["responseEtag"],
-            "Cache-Control": "must-revalidate"
-        })
+        return self.getList(
+            request=request,
+            assetId=assetId,
+            domain=domain,
+            actionCallback=actionCallback,
+            permission={
+                "args": {
+                    "assetId": assetId,
+                    "domain": domain
+                }
+            }
+        )
 
 
 
     def post(self, request: Request, assetId: int, domain: str, packageUid: str) -> Response:
-        response = dict()
-        user = CustomControllerBase.loggedUser(request)
-
-        try:
-            if Permission.hasUserPermission(groups=user["groups"], action="nat_rulebase_post", assetId=assetId, domain=domain) or user["authDisabled"]:
-                Log.actionLog("NAT rule addition", user)
-                Log.actionLog("User data: "+str(request.data), user)
-
-                serializer = Serializer(data=request.data["data"], partial=True)
-                if serializer.is_valid():
-                    data = serializer.validated_data
-
-                    lock = Lock("nat_rule", locals())
-                    if lock.isUnlocked():
-                        lock.lock()
-
-                        response["data"] = PolicyPackage.addNatRule(sessionId=self.sessionId, assetId=assetId, domain=domain, policyPackageUid=packageUid, data=data)
-                        httpStatus = status.HTTP_201_CREATED
-                        lock.release()
-                    else:
-                        httpStatus = status.HTTP_423_LOCKED
-                else:
-                    httpStatus = status.HTTP_400_BAD_REQUEST
-                    response = {
-                        "CheckPoint": {
-                            "error": str(serializer.errors)
-                        }
-                    }
-                    Log.actionLog("User data incorrect: "+str(response), user)
-            else:
-                httpStatus = status.HTTP_403_FORBIDDEN
-        except Exception as e:
-            Lock("nat_rule", locals()).release()
-
-            data, httpStatus, headers = CustomControllerBase.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-        finally:
-            try:
-                time.sleep(0.5)
-                Session(sessionId=self.sessionId, assetId=assetId, domain=domain).logout()
-            except Exception:
-                pass
-
-        return Response(response, status=httpStatus, headers={
-            "Cache-Control": "no-cache"
-        })
+        return self.create(
+            request=request,
+            assetId=assetId,
+            domain=domain,
+            Serializer=Serializer,
+            actionCallback=lambda data: PolicyPackage.addNatRule(sessionId=self.sessionId, assetId=assetId, domain=domain, policyPackageUid=packageUid, data=data),
+            permission={
+                "args": {
+                    "assetId": assetId,
+                    "domain": domain
+                }
+            }
+        )
