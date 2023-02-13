c793f4e0638d2b47d876315a8a4ae35fbd938231
mik <m.sartori@lumit.it>
2023-02-13 14:57:00 +0100

    HostSerializer

--- checkpoint/serializers/CheckPoint/Host.py
+++ checkpoint/serializers/CheckPoint/Host.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
-from checkpoint.helpers.Utils import CheckString
+from checkpoint.helpers.Exception import CustomException
 from checkpoint.serializers.CheckPoint.CommonDataStruct import CheckPointNatSettingsSerializer
+from checkpoint.helpers.Log import Log
+
+
+class CheckPointHostAddRemoveFieldSerializer(serializers.Serializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for k in kwargs["data"]:
+            if k in ("add", "remove"):
+                if isinstance(kwargs["data"][k], list):
+                    self.fields[k] = serializers.ListField(child=serializers.CharField(max_length=64, required=False), required=False)
+                elif isinstance(kwargs["data"][k], str):
+                    self.fields[k] = serializers.CharField(max_length=64, required=False, allow_blank=True)
+                else:
+                    raise CustomException(status=400, payload={"CheckPoint": "Invalid data for key \"" + k + "\". Expected a list or a string."})
+            else:
+                raise CustomException(status=400, payload={"CheckPoint": "Invalid data. Expected dictionary key: \"add\" or \"remove\", got \"" + k + "\"."})
+
 
 
 class CheckPointHostWebServerConfigSerializer(serializers.Serializer):
@@ -11,50 +29,41 @@ class CheckPointHostWebServerConfigSerializer(serializers.Serializer):
         self.fields["operating-system"] = serializers.CharField(max_length=64, required=False, allow_blank=True)
         self.fields["protected-by"] = serializers.CharField(max_length=255, required=False, allow_blank=True)
 
-        self.fields["additional-ports"] = serializers.JSONField(required=False)
-        self.fields["application-engines"] = serializers.JSONField(required=False)
+        # These fields can be either a string, a list of string or a dict.
+        if "additional-ports" in kwargs["data"]:
+            if isinstance(kwargs["data"]["additional-ports"], str):
+                self.fields["additional-ports"] =  serializers.CharField(max_length=255, required=False)
 
-    def to_internal_value(self, data):
-        def checkAddRemoveField(d: dict):
-            for k in d:
-                if k != "add" and k != "remove":
-                    raise ValueError('Invalid data. Expected key: "add" or "remove".')
-                if isinstance(d[k], str):
-                    d[k] = CheckString.check(d[k], 15)
-                elif isinstance(d[k], list):
-                    d[k] = [CheckString.check(el, 15) for el in d[k]]
-                else:
-                    raise serializers.ValidationError('Invalid data in "add" or "remove" field.')
+            elif isinstance(kwargs["data"]["additional-ports"], list):
+                self.fields["additional-ports"] = serializers.ListField(child=serializers.CharField(max_length=255, required=False), required=False)
 
-        try:
-            # These fields can be a string, a list of string or a dict.
-            if "additional-ports" in data:
-                if isinstance(data["additional-ports"], str):
-                    data["additional-ports"] = CheckString.allowedChars(data["additional-ports"], 15, "0123456789")
-                elif isinstance(data["additional-ports"], list):
-                    data["additional-ports"] = [ CheckString.allowedChars(el, 15, "0123456789") for el in data["additional-ports"]]
-                elif isinstance(data["additional-ports"], dict):
-                    data["additional-ports"] = checkAddRemoveField(data["additional-ports"])
-                else:
-                    msg = 'Incorrect type. Expected a string, list or a dict, but got %s'
-                    raise serializers.ValidationError(msg % type(data["additional-ports"]).__name__)
+            elif isinstance(kwargs["data"]["additional-ports"], dict):
+                AdditionalPortsAddRemoveFieldArgs = dict()
+                AdditionalPortsAddRemoveFieldArgs["data"] = kwargs.get("data", {}).get("additional-ports", {})
+                AdditionalPortsAddRemoveFieldArgs["required"] = False
+                self.fields["additional-ports"] = CheckPointHostAddRemoveFieldSerializer(**AdditionalPortsAddRemoveFieldArgs)
 
-            if "application-engines" in data:
-                if isinstance(data["application-engines"], str):
-                    data["application-engines"] = CheckString.check(data["application-engines"], 15)
-                elif isinstance(data["application-engines"], list):
-                    data["application-engines"] = [CheckString.check(el, 15) for el in data["application-engines"]]
-                elif isinstance(data["application-engines"], dict):
-                    data["application-engines"] = checkAddRemoveField(data["application-engines"])
-                else:
-                    msg = 'Incorrect type. Expected a string, list or a dict, but got %s'
-                    raise serializers.ValidationError(msg % type(data["application-engines"]).__name__)
+            else:
+                msg = 'Incorrect type. Expected a string, list or a dict, but got %s'
+                raise serializers.ValidationError(msg % type(kwargs["data"]["additional-ports"]).__name__)
 
-            return data
-        except ValueError as v:
-            raise serializers.ValidationError from v
-        except Exception as e:
-            raise e
+
+        if "application-engines" in kwargs["data"]:
+            if isinstance(kwargs["data"]["application-engines"], str):
+                self.fields["application-engines"] = serializers.CharField(max_length=255, required=False)
+
+            elif isinstance(kwargs["data"]["application-engines"], list):
+                self.fields["application-engines"] = serializers.ListField(child=serializers.CharField(max_length=255, required=False), required=False)
+
+            elif isinstance(kwargs["data"]["application-engines"], dict):
+                ApplicationEnginesAddRemoveFieldArgs = dict()
+                ApplicationEnginesAddRemoveFieldArgs["data"] = kwargs.get("data", {}).get("application-engines", {})
+                ApplicationEnginesAddRemoveFieldArgs["required"] = False
+                self.fields["application-engines"] = CheckPointHostAddRemoveFieldSerializer(**ApplicationEnginesAddRemoveFieldArgs)
+
+            else:
+                msg = 'Incorrect type. Expected a string, list or a dict, but got %s'
+                raise serializers.ValidationError(msg % type(kwargs["data"]["application-engines"]).__name__)
 
 
 
@@ -62,21 +71,30 @@ class CheckPointHostServerSerializer(serializers.Serializer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        WebServerConfigArg = dict()
+        WebServerConfigArg["data"] = kwargs.get("data", {}).get("web-server-config", {})
+        WebServerConfigArg["required"] = False
+
         self.fields["dns-server"] = serializers.BooleanField(required=False)
         self.fields["mail-server"] = serializers.BooleanField(required=False)
         self.fields["web-server"] = serializers.BooleanField(required=False)
-        self.fields["web-server-config"] =  CheckPointHostWebServerConfigSerializer(required=False)
+        self.fields["web-server-config"] =  CheckPointHostWebServerConfigSerializer(**WebServerConfigArg)
+
 
 
 class CheckPointHostSerializer(serializers.Serializer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        HostServerConfigArgs = dict()
+        HostServerConfigArgs["data"] = kwargs.get("data", {}).get("host-servers", {})
+        HostServerConfigArgs["required"] = False
+
         self.fields["ipv4-address"] = serializers.IPAddressField(required=True)
         self.fields["ipv6-address"] = serializers.IPAddressField(required=False)
         self.fields["nat-settings"] = CheckPointNatSettingsSerializer(required=False)
         self.fields["new-name"] = serializers.CharField(max_length=255, required=False)
-        self.fields["host-servers"] = CheckPointHostServerSerializer(required=False)
+        self.fields["host-servers"] = CheckPointHostServerSerializer(**HostServerConfigArgs)
 
         self.fields["set-if-exists"] = serializers.BooleanField(required=False)
         self.fields["details-level"] = serializers.CharField(max_length=64, required=False)
