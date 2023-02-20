d887bed1d1efdeba93a6c3f515b8590f0a221a17
Marco Buratto <m.buratto@lumit.it>
2023-02-20 12:36:10 +0100

    Datacenter server modification async task handling

--- checkpoint/models/CheckPoint/backend/DatacenterServer.py
+++ checkpoint/models/CheckPoint/backend/DatacenterServer.py
@@ -35,13 +35,37 @@ class DatacenterServer:
     @staticmethod
     def modify(sessionId: str, assetId: int, domain: str, uid: str, data: dict, autoPublish: bool = True) -> None:
         data["uid"] = uid
+        timeout = 120 # [second]
+
+        from checkpoint.models.CheckPoint.Task import Task
 
         try:
-            ApiSupplicant(sessionId, assetId).post(
+            response = ApiSupplicant(sessionId, assetId).post(
                 urlSegment="set-data-center-server",
                 domain=domain,
                 data=data
             )
+
+            # Monitor async tasks.
+            t0 = time.time()
+
+            while True:
+                try:
+                    taskRunInfo = Task(sessionId, assetId, domain, uid=response["task-id"]).info()["tasks"][0]
+
+                    if taskRunInfo["status"] == "succeeded":
+                        break
+                    elif taskRunInfo["status"] == "failed":
+                        raise CustomException(status=400, payload={"CheckPoint": taskRunInfo.get("task-details", [])[0].get("fault-message", "unknown error")})
+
+                    if time.time() >= t0 + timeout: # timeout reached.
+                        raise CustomException(status=400, payload={"CheckPoint": f"task timeout reached"})
+
+                    time.sleep(5)
+                except KeyError:
+                    pass
+                except IndexError:
+                    pass
 
             if autoPublish:
                 Session(sessionId=sessionId, assetId=assetId, domain=domain).publish()
