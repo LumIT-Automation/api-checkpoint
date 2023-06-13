d3337699fbdc62deb313b5dc235bde7183a7d00e
mik <m.sartori@lumit.it>
2023-06-13 17:24:12 +0200

    Asset: non-mandatory fields

--- checkpoint/serializers/CheckPoint/Asset/Assets.py
+++ checkpoint/serializers/CheckPoint/Asset/Assets.py
@@ -4,4 +4,4 @@ from checkpoint.serializers.CheckPoint.Asset.Asset import CheckPointAssetSeriali
 
 
 class CheckPointAssetsSerializer(serializers.Serializer):
-    items = CheckPointAssetSerializer(many=True)
+    items = CheckPointAssetSerializer(idField=True, many=True)
